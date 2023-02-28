ddaf5445bfe890bab312f275f97b5d7c8a24921d
MAROPENG MARTHA <104618684+MARTHAMOGALE@users.noreply.github.com>
2023-02-28 12:20:05 +0200

    Add project files.

---
+++ CapenexisLearners/Data/CapenexisLearnersContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CapenexisLearners23.Models;
+
+namespace CapenexisLearners.Data
+{
+    public class CapenexisLearnersContext : DbContext
+    {
+        public CapenexisLearnersContext (DbContextOptions<CapenexisLearnersContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CapenexisLearners23.Models.Learners> Learners { get; set; } = default!;
+
+        public DbSet<CapenexisLearners23.Models.Courses> Courses { get; set; } = default!;
+
+        public DbSet<CapenexisLearners23.Models.Facilitators> Facilitators { get; set; } = default!;
+    }
+}
